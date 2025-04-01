6b3e93ff0f913c3a9dffaff57d84ae332591f5a2
zxsheather <zxsheather@sjtu.edu.cn>
2025-04-01 23:01:12 +0800

    feat:add submission monitor to monitor submission more precisely and thus fix the cache issue where the cache was not being cleared when the token was changed.

--- src/commands.ts
+++ src/commands.ts
@@ -8,6 +8,7 @@ import { exec } from 'child_process' // Node.js module to run shell commands
 import { promisify } from 'util' // To use async/await with exec
 import * as path from 'path' // Node.js module for path manipulation
 import { get } from 'axios'
+import { SubmissionMonitorService } from './submissionMonitor'
 
 const execAsync = promisify(exec) // used in getGitRemoteFetchUrls
 
@@ -17,6 +18,7 @@ export function registerCommands(
   apiClient: ApiClient,
   problemsetProvider: ProblemsetProvider,
   submissionProvider: SubmissionProvider,
+  submissionMonitor: SubmissionMonitorService,
 ) {
   context.subscriptions.push(
     vscode.commands.registerCommand('acmoj.setToken', async () => {
@@ -327,6 +329,7 @@ export function registerCommands(
               )
               submissionProvider.refresh()
               // setTimeout(() => vscode.commands.executeCommand('acmoj.viewSubmission', result.id), 3000);
+              submissionMonitor.addSubmission(result.id)
             } catch (error: any) {
               vscode.window.showErrorMessage(
                 `Submission failed: ${error.message}`,
